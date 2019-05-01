14cbecb08f8c3a13a70e4ca9f7046b9eb31e5114
Nikita Novozhilov <Nikita_Novozhilov@epam.com>
2019-05-01 11:04:47 +0400

    init

---
+++ src/main/java/com/piano/test/stackoverflowapi/StackoverflowapiApplication.java
@@ -0,0 +1,13 @@
+package com.piano.test.stackoverflowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackoverflowapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackoverflowapiApplication.class, args);
+	}
+
+}
